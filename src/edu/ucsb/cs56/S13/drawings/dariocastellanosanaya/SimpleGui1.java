638ea602a85c05019c79a05a24319474998ed253
Dario Castellanos <dariocastellanosanaya@umail.ucsb.edu>
2013-05-22 10:29:19 -0700

    done with Step 5

---
+++ src/edu/ucsb/cs56/S13/drawings/dariocastellanosanaya/SimpleGui1.java
@@ -0,0 +1,30 @@
+package edu.ucsb.cs56.S13.drawings.dariocastellanosanaya;
+import javax.swing.*;
+
+/** SimpleGui1 comes from Head First Java 2nd Edition p. 355.
+      It illustrates a simple GUI with a Button that doesn't do anything yet.
+
+     @author Head First Java, 2nd Edition p. 355
+     @author P. Conrad (who only typed it in and added the Javadoc comments)
+     @author Dario Castellanos Anaya
+     @version CS56, Spring 2013, UCSB
+*/
+
+public class SimpleGui1 {
+
+    /** main method to fire up a JFrame on the screen  
+          @param args not used
+    */
+
+    public static void main (String[] args) {
+	JFrame frame = new JFrame() ;
+	JButton button = new JButton("click me if you dare") ;
+	java.awt.Color myColor = new java.awt.Color(102,153,255);   // R, G, B values.
+	button.setBackground(myColor);
+	button.setOpaque(true);
+	frame. setDefaultCloseOperation(JFrame. EXIT_ON_CLOSE) ;
+	frame. getContentPane() . add(button) ;
+	frame. setSize(300,300) ;
+	frame. setVisible(true) ;
+    }
+}

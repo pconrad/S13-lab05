aee98f91824813460a5e5031701e97809032918d
Dario Castellanos <dariocastellanosanaya@umail.ucsb.edu>
2013-05-24 06:21:45 -0700

    Done making the drawings

--- src/edu/ucsb/cs56/S13/drawings/dariocastellanosanaya/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/S13/drawings/dariocastellanosanaya/advanced/AllMyDrawings.java
@@ -11,6 +11,7 @@ import java.awt.Shape; // general class for shapes
 import java.awt.Color; // class for Colors
 import java.awt.Stroke;
 import java.awt.BasicStroke;
+import java.awt.Font;
 
 
 import edu.ucsb.cs56.S13.drawings.utilities.ShapeTransforms;
@@ -27,83 +28,76 @@ import edu.ucsb.cs56.S13.drawings.utilities.GeneralPathWrapper;
 
 public class AllMyDrawings
 {
-    /** Draw a picture with a few ghosts 
+    /** Draw Blinky, Pinky, Inky and Clyde 
      */
 
     public static void drawPicture1(Graphics2D g2) {
 
-	Ghost ghost1 = new Ghost(100,250,50,50);
-	g2.setColor(Color.CYAN); g2.draw(ghost1);
-	
-	// Make a black ghost that's half the size, 
-	// and moved over 150 pixels in x direction
-
-	Shape ghost2 = ShapeTransforms.scaledCopyOfLL(ghost1,0.5,0.5);
-	ghost2 = ShapeTransforms.translatedCopyOf(ghost2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(ghost2);
-	
-	// Here's a ghost that's 4x as big (2x the original)
-	// and moved over 150 more pixels to right.
-	ghost2 = ShapeTransforms.scaledCopyOfLL(ghost2,4,4);
-	ghost2 = ShapeTransforms.translatedCopyOf(ghost2,150,0);
-	
-	// We'll draw this with a thicker stroke
-	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
-	
-	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
-	// #002FA7 is "International Klein Blue" according to Wikipedia
-	// In HTML we use #, but in Java (and C/C++) its 0x
-	
-	Stroke orig=g2.getStroke();
+	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);   
 	g2.setStroke(thick);
-	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(ghost2); 
+	int fontSize= 20;
+	Font original = g2.getFont();
+	g2.setFont(new Font("TimesRoman", Font.BOLD, fontSize));
+
+	// Blinky 
+
+	Ghost blinky = new Ghost(100,50,80,80);
+	g2.setColor(Color.RED); g2.draw(blinky);
+	g2.drawString("\"BLINKY\"", 250,100);
+
+	// Pinky, moved 100 pixels down
+
+	Shape pinky = ShapeTransforms.translatedCopyOf(blinky,0,100);
+	g2.setColor(Color.PINK); g2.draw(pinky);
+	g2.drawString("\"PINKY\"", 250,200);
 	
-	// Draw two scared ghosts
+	// Inky, moved 100 more pixels down
+	Shape inky = ShapeTransforms.translatedCopyOf(pinky,0,100);
+	g2.setColor(Color.CYAN); g2.draw(inky);
+	g2.drawString("\"INKY\"", 250,300);
 	
-	ScaredGhost sg1 = new ScaredGhost(50,350,40,40);
-	ScaredGhost sg2 = new ScaredGhost(150,50,200,200);
-	
-	g2.draw(sg1);
-	g2.setColor(new Color(0x8F00FF)); g2.draw(sg2);
+	// Clyde, moved another 100 pixels down
+	Shape clyde = ShapeTransforms.translatedCopyOf(inky,0,100);
+	g2.setColor(Color.ORANGE); g2.draw(clyde);
+	g2.drawString("\"CLYDE\"", 250,400);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
-	g2.setStroke(orig);
+	g2.setFont(original);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A few ghosts by Dario Castellanos", 20,20);
+	g2.drawString("Blinky, Pinky, Inky and Clyde by Dario Castellanos", 20,20);
     }
 
 
-    /** Draw a picture with a few houses and coffee cups
+    /** Draw a picture with a few ghosts
      */
     public static void drawPicture2(Graphics2D g2) {
 
-	// Draw some coffee cups.
+	// Draw some ghosts.
 	
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
-	CoffeeCup tallSkinny = new CoffeeCup(20,150,20,40);
-	CoffeeCup shortFat = new CoffeeCup(20,250,40,20);
+	Ghost large = new Ghost(100,50,225,150);
+	Ghost smallGh = new Ghost(20,50,40,30);
+	Ghost  tallSkinny = new Ghost(20,150,20,40);
+	Ghost shortFat = new Ghost(20,250,40,20);
 	
 	g2.setColor(Color.RED);     g2.draw(large);
-	g2.setColor(Color.GREEN);   g2.draw(smallCC);
+	g2.setColor(Color.GREEN);   g2.draw(smallGh);
 	g2.setColor(Color.BLUE);    g2.draw(tallSkinny);
 	g2.setColor(Color.MAGENTA); g2.draw(shortFat);
 	
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
+	Ghost gh1 = new Ghost(100,250,50,75);
+	g2.setColor(Color.CYAN); g2.draw(gh1);
 	
-	// Make a black house that's half the size, 
+	// Make a black ghost that's half the size, 
 	// and moved over 150 pixels in x direction
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+	Shape gh2 = ShapeTransforms.scaledCopyOfLL(gh1,0.5,0.5);
+	gh2 = ShapeTransforms.translatedCopyOf(gh2,150,0);
+	g2.setColor(Color.BLACK); g2.draw(gh2);
 	
-	// Here's a house that's 4x as big (2x the original)
-	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+	// Here's a Ghost that's 4x as big (2x the original)
+	// and moved over 150 more pixels to the right.
+	gh2 = ShapeTransforms.scaledCopyOfLL(gh2,4,4);
+	gh2 = ShapeTransforms.translatedCopyOf(gh2,150,0);
 	
 	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
@@ -115,46 +109,60 @@ public class AllMyDrawings
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
+	g2.draw(gh2); 
 	
-	// Draw two houses with Windows
+	// Draw two scared ghosts
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+	ScaredGhost sg1 = new ScaredGhost(50,350,40,75);
+	ScaredGhost sg2 = new ScaredGhost(200,350,200,100);
 	
-	g2.draw(hw1);
+	g2.draw(sg1);
 	g2.setColor(new Color(0x8F00FF)); 
 
-	// Rotate the second house 45 degrees around its center.
-	Shape hw3 = ShapeTransforms.rotatedCopyOf(hw2, Math.PI/4.0);
+	// Rotate the second ghost 45 degrees around its center.
+	Shape sg3 = ShapeTransforms.rotatedCopyOf(sg2, Math.PI/4.0);
 
-	g2.draw(hw3);
+	g2.draw(sg3);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A bunch of Coffee Cups and a few houses by Phill Conrad", 20,20);
+	g2.drawString("A bunch of regular and scared Ghosts by Dario Castellanos", 20,20);
     }
   
-    /** Draw a different picture with a few houses and coffee cups
+    /** Draw a picture of the four ghosts being scared
      */
 
     public static void drawPicture3(Graphics2D g2) {
 	
 	// label the drawing
 	
-	g2.drawString("A bunch of Coffee Cups by Phill Conrad", 20,20);
+	g2.drawString("\"Oh no! They got Clyde!\" by Dario Castellanos", 20,20);
+	
+	// change stroke and font
 
-	
-	// Draw some coffee cups.
-	
-       CoffeeCup large = new CoffeeCup(100,50,225,150);
-       CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
-       
-       g2.setColor(Color.RED);     g2.draw(large);
-       g2.setColor(Color.GREEN);   g2.draw(smallCC);
-       
+	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);   
+	g2.setStroke(thick);
+	int fontSize= 60;
+	g2.setFont(new Font("TimesRoman", Font.PLAIN, fontSize));
+
+	// draw the three ghosts
+
+	g2.setColor(Color.BLUE);
+
+	ScaredGhost sg1 = new ScaredGhost(220,200,100,100);
+	g2.draw(sg1);
+
+	Shape sg2  = ShapeTransforms.translatedCopyOf(sg1,120,0);
+	g2.draw(sg2);
+
+	sg2  = ShapeTransforms.translatedCopyOf(sg2,120,0);
+	g2.draw(sg2);
+
+	// draw Clyde
+	g2.setColor(Color.CYAN);
+        g2.drawString("200", 90, 270);
        
     }
     
